4813eb29dba7d56a7aa2fe5b24bc7c501153047b
Omer Akgul <omer@wireless-10-104-161-18.umd.edu>
2020-02-12 15:23:24 -0500

    no more sms option

--- src/org/securityed/securesms/ApplicationPreferencesActivity.java
+++ src/org/securityed/securesms/ApplicationPreferencesActivity.java
@@ -54,7 +54,14 @@ import org.w3c.dom.Text;
 /**
  * The Activity for application preference display and management.
  *
- * @author Moxie Marlinspike
+ *  @author Moxie Marlinspike
+ *
+ * we don't want to mess with sms so that's disabled.
+ * we want an option to learn more about e2ee so that's added.
+ *
+ * Omer
+ *
+ *
  *
  */
 
@@ -65,7 +72,7 @@ public class ApplicationPreferencesActivity extends PassphraseRequiredActionBarA
   private static final String TAG = ApplicationPreferencesActivity.class.getSimpleName();
 
   private static final String PREFERENCE_CATEGORY_PROFILE        = "preference_category_profile";
-  private static final String PREFERENCE_CATEGORY_SMS_MMS        = "preference_category_sms_mms";
+  //private static final String PREFERENCE_CATEGORY_SMS_MMS        = "preference_category_sms_mms";
   private static final String PREFERENCE_CATEGORY_NOTIFICATIONS  = "preference_category_notifications";
   private static final String PREFERENCE_CATEGORY_APP_PROTECTION = "preference_category_app_protection";
   private static final String PREFERENCE_CATEGORY_APPEARANCE     = "preference_category_appearance";
@@ -145,8 +152,8 @@ public class ApplicationPreferencesActivity extends PassphraseRequiredActionBarA
 
       this.findPreference(PREFERENCE_CATEGORY_PROFILE)
           .setOnPreferenceClickListener(new ProfileClickListener());
-      this.findPreference(PREFERENCE_CATEGORY_SMS_MMS)
-        .setOnPreferenceClickListener(new CategoryClickListener(PREFERENCE_CATEGORY_SMS_MMS));
+      //this.findPreference(PREFERENCE_CATEGORY_SMS_MMS)
+      //  .setOnPreferenceClickListener(new CategoryClickListener(PREFERENCE_CATEGORY_SMS_MMS));
       this.findPreference(PREFERENCE_CATEGORY_NOTIFICATIONS)
         .setOnPreferenceClickListener(new CategoryClickListener(PREFERENCE_CATEGORY_NOTIFICATIONS));
       this.findPreference(PREFERENCE_CATEGORY_APP_PROTECTION)
@@ -190,8 +197,8 @@ public class ApplicationPreferencesActivity extends PassphraseRequiredActionBarA
     private void setCategorySummaries() {
       ((ProfilePreference)this.findPreference(PREFERENCE_CATEGORY_PROFILE)).refresh();
 
-      this.findPreference(PREFERENCE_CATEGORY_SMS_MMS)
-          .setSummary(SmsMmsPreferenceFragment.getSummary(getActivity()));
+      //this.findPreference(PREFERENCE_CATEGORY_SMS_MMS)
+      //    .setSummary(SmsMmsPreferenceFragment.getSummary(getActivity()));
       this.findPreference(PREFERENCE_CATEGORY_NOTIFICATIONS)
           .setSummary(NotificationsPreferenceFragment.getSummary(getActivity()));
       this.findPreference(PREFERENCE_CATEGORY_APP_PROTECTION)
@@ -232,7 +239,7 @@ public class ApplicationPreferencesActivity extends PassphraseRequiredActionBarA
       DrawableCompat.setTint(devices, color);
       DrawableCompat.setTint(advanced, color);
 
-      this.findPreference(PREFERENCE_CATEGORY_SMS_MMS).setIcon(sms);
+      //this.findPreference(PREFERENCE_CATEGORY_SMS_MMS).setIcon(sms);
       this.findPreference(PREFERENCE_CATEGORY_NOTIFICATIONS).setIcon(notifications);
       this.findPreference(PREFERENCE_CATEGORY_APP_PROTECTION).setIcon(privacy);
       this.findPreference(PREFERENCE_CATEGORY_APPEARANCE).setIcon(appearance);
@@ -256,9 +263,9 @@ public class ApplicationPreferencesActivity extends PassphraseRequiredActionBarA
         Fragment fragment = null;
 
         switch (category) {
-        case PREFERENCE_CATEGORY_SMS_MMS:
-          fragment = new SmsMmsPreferenceFragment();
-          break;
+        //case PREFERENCE_CATEGORY_SMS_MMS:
+        //  fragment = new SmsMmsPreferenceFragment();
+        //  break;
         case PREFERENCE_CATEGORY_NOTIFICATIONS:
           fragment = new NotificationsPreferenceFragment();
           break;
